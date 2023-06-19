a331c303913e56ef3ea0b1d0c959dd4f3ca7309e
Nitish Kumar Erelli <123429249+nitishkumar2306@users.noreply.github.com>
2023-06-19 17:22:12 -0500

    Update main.py

--- Assignments/A07/main.py
+++ Assignments/A07/main.py
@@ -25,12 +25,13 @@ def currentDate(returnType='tuple'):
     else:
         raise ValueError("Invalid returnType. Valid values are 'tuple', 'list', or 'dict'.")
 
-def gui():
-    # Create a list of months, days, and years
-    months = list(range(1, 13))
-    days = list(range(1, 32))
-    currentYear = currentDate()
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
+def gui():
+    # Create a list of months, years, and filter options
+    months = list(range(1, 13))
+    currentYear = currentDate()
     years = list(range(2000, currentYear[2] + 1))
     filter_options = ['daily', 'weekly', 'monthly']
 
@@ -38,8 +39,8 @@ def gui():
     layout = [
         [sg.Text('Select a date:')],
         [
-            [sg.Text('Month:')], [sg.Combo(months, key='-MONTH-', size=(30, 1), readonly=True)],
-            [sg.Text('Day:')], [sg.Combo(days, key='-DAY-', size=(30, 1), readonly=True)],
+            [sg.Text('Month:')], [sg.Combo(months, key='-MONTH-', size=(30, 1), readonly=True, enable_events=True)],
+            [sg.Text('Day:')], [sg.Combo([], key='-DAY-', size=(30, 1), readonly=True)],
             [sg.Text('Year:')], [sg.Combo(years, key='-YEAR-', size=(30, 1), readonly=True)],
             [sg.Text('Code:')], [sg.Combo(codes, key='-CODE-', size=(30, 1), readonly=True)],
             [sg.Text('Filter:')], [sg.Combo(filter_options, key='-FILTER-', size=(30, 1), readonly=True)]
@@ -58,28 +59,35 @@ def gui():
         # Handle events
         if event == sg.WINDOW_CLOSED or event == 'Cancel':
             break
+        elif event == '-MONTH-':
+            month = int(values['-MONTH-'])
+            year = int(values['-YEAR-'])
+            
+            if month == 2:
+                num_days = 29 if is_leap_year(year) else 28
+            else:
+                num_days = monthrange(year, month)[1]
+            
+            window['-DAY-'].update(values=list(range(1, num_days + 1)))
         elif event == 'Submit':
-            month = values['-MONTH-']
-            day = values['-DAY-']
-            year = values['-YEAR-']
+            month = int(values['-MONTH-'])
+            day = int(values['-DAY-'])
+            year = int(values['-YEAR-'])
             code = values['-CODE-']
             filter = values['-FILTER-']
             selected_date = f'{month}/{day}/{year}'
 
             # Create a custom result window
             result_layout = [
-                [sg.Text(f'You selected\nDate:{selected_date}\nCode: {code}\nFilter: {filter}',justification='center')],
+                [sg.Text(f'You selected\nDate: {selected_date}\nCode: {code}\nFilter: {filter}', justification='center')],
                 [sg.Button('OK')]
             ]
             result_window = sg.Window('Details', result_layout, size=(600, 200), element_justification='center')
 
             base_url = "https://www.wunderground.com/history"
-            # build the url to scrape weather from
-            # url = f"{base_url}/{filter}/{code}/date/{year}-{month}-{day}"
+            # Build the URL to scrape weather from
             url = f"{base_url}/{filter}/{code}/date/{year}-{month}-{day}"
-            # print(url)
             
-                        
             while True:
                 event, _ = result_window.read()
                 
@@ -91,9 +99,27 @@ def gui():
     
     window.close()
 
+def ShowData(history):
+     # Extract table headers and rows from the data element
+    headers = [header.get_text(strip=True) for header in history.find_all('th')]
+    rows = []
+    for row in history.find_all('tr'):
+        rows.append([data.get_text(strip=True) for data in row.find_all('td')])
+
+    # Create PySimpleGUI layout for the table
+    # Create PySimpleGUI layout for the table with adjusted column widths
+    layout = [
+    [sg.Table(values=rows, headings=headers, justification='left', col_widths=[20, 20])]
+    ]
+
+    # Create PySimpleGUI window and display the table
+    window = sg.Window('HTML Table', layout)
+    event, values = window.read()
+    window.close()
+
 
 if __name__ == '__main__':
     url = gui()
-    print(url)
-    print(type(url))
-    get_weather.mainfunction(url)
+    bs4_data = get_weather.mainfunction(url)
+    ShowData(bs4_data)
+
